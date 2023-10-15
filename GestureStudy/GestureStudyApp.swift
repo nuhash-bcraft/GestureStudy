443b58cc60a0d5cd2c6fbae3330c9081806d5591
afnan <afnan@braincraftapps.com>
2023-10-15 18:39:35 +0600

    Initial Commit

---
+++ GestureStudy/GestureStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureStudyApp.swift
+//  GestureStudy
+//
+//  Created by BCL Device 18 on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GestureStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
